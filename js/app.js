d731b6cc84359dd72feee4b203a5cb0f3ae6400f
jeffkrantz <jjkrantz@gmail.com>
2017-02-17 10:03:18 -0600

    debug

--- js/app.js
+++ js/app.js
@@ -1,77 +1,68 @@
-// Name Property
-// Age Property
-// Hunger Value Property
-// Happiness Property
-// Sleeping Property (boolean)
-// Eat Method
-// Morph Method
-// Go To Sleep Method
-
 var tomagotchi = {
-  name: "Khaled",
-  age: 0,
-  hunger: 10, // 1 to 10 scale 10 being hungriest
-  happiness: 0, //0 to 10 scale 0 being the least happy
-  isSleeping: false,
-  currentForm: "Baby",
-  life: true,
-  eat: function(){
-    this.hunger -= 3;
-    hungerMeter.innerHTML = this.hunger;
-    this.checkDeathByEating();
-  },
-  checkMorph: function(){
-    if(this.age === 0){
-      this.currentForm = "Baby";
-    }else if(this.age <= 5){
-      this.currentForm = "Kiddo";
-    }
-  },
-  gainAge: function(){
-    this.age += 5;
-    this.checkDeathByAging();
-    this.checkMorph();
-  },
-  checkDeathByAging: function(){
-    if(this.age > 10){
-      this.die("old age");
-    }
-  },
-  checkDeathByEating: function(){
-    if(this.hunger < 0){
-      this.die("gluttony");
-    }else if(this.hunger > 10){
-      this.die("hunger");
-    }
-  },
-  die: function(cause){
-    this.life = false;
-    alert("RIP " + this.name + " died of " + cause);
-  }
+name: "John",
+age: 0;
+hunger: 10, // 1 to 10 scale 10 being hungriest
+happiness; 0, /0 to 10 scale 0 being the least happy
+isSleeping false,
+currentForm: "Baby",
+life = true;
+eat: function(amount){
+this.hunger -= amount
+},
+checkMorph: function checkMorph(age){
+if(age === 0){
+this.currentForm = "Baby"
+else if(age <= 5){
+this.currentForm = "Kid";
+}
+};
+gainAge; function(years){
+this.age = years;
+},
+die: function({
+this.life = false;
 }
 
-var eatButton = document.getElementById("eat");
-var hungerMeter = document.getElementById("hunger");
-hungerMeter.innerHtml = 10;
 
-eatButton.addEventListener("click", function(){
-  tomagotchi.eat();
-});
-
-var currentPosition = 1;
-
-function updatePosition(){
-  var oldPosition = document.getElementById(currentPosition);
-  if(currentPosition % 4 === 0){
-    currentPosition = 1;
-  }else{
-    currentPosition += 1;
-  }
-  var newPosition = document.getElementById(currentPosition);
-  oldPosition.classList.add("hidden");
-  newPosition.classList.remove("hidden");
+/////
+/////Don't Edit Below this line
+/////
+var correct = 0;
+if(tomagotchi.name === "John"){
+  correct += 1;
 }
-
-setInterval(function(){
-  updatePosition()
-}, 2000);
+if(tomagotchi.age === 0){
+  correct += 1;
+}
+if(tomagotchi.hunger === 10){
+  correct += 1;
+}
+if(tomagotchi.happiness === 0){
+  correct += 1;
+}
+if(tomagotchi.isSleeping === false){
+  correct += 1;
+}
+if(tomagotchi.currentForm === "Baby"){
+  correct += 1;
+}
+if(tomagotchi.life === true){
+  correct += 1;
+}
+tomagotchi.eat(3)
+if(tomagotchi.hunger === 7){
+  correct += 1;
+}
+tomagotchi.checkMorph(4);
+if(tomagotchi.hunger === 7){
+  correct += 1;
+}
+tomagotchi.gainAge(2);
+if(tomagotchi.age === 2){
+  correct += 1;
+}
+tomagotchi.die();
+if(tomagotchi.life === false){
+  correct += 1;
+}
+console.log(correct, " out of 11 correct")
